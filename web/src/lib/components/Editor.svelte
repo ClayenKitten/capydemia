20be12d9f56446a39ea1e536bcfa66d7afa8f75a
domik <astranastya_1@mail.ru>
2024-08-19 11:51:15 +0000

    editorJS edited Co-authored-by: ClayenKitten <ClayenKitten@users.noreply.github.com>

---
+++ web/src/lib/components/Editor.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let holder: HTMLElement;
+    
+    onMount(async () => {		
+		const EditorJS = await import('@editorjs/editorjs');
+		const Header = await import('@editorjs/header');
+		const List = await import('@editorjs/list');
+
+		let editor: EditorJS.default;
+		editor = new EditorJS.default({
+			holder: "editorjs",
+			tools: {
+				header: Header.default,
+				list: List.default
+			},
+			placeholder: 'Let`s write an awesome story!',
+			//readOnly: true
+		});
+    }) 
+</script>
+
+<div bind:this={holder} />
--- web/src/routes/(main)/course/[courseId]/lesson/[lessonId]/+page.svelte
+++ web/src/routes/(main)/course/[courseId]/lesson/[lessonId]/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import EditorJS from "$lib/components/Editor.svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
 </script>
 
-<div class="editorjs"></div>
+<div class="editorjs">
+	<EditorJS>
+		
+	</EditorJS>
+</div>
 <h5>Конспект урока</h5>
 <span>{data.lessonContent}</span>
