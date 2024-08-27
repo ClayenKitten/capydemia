2548431de13e01fee01fdf31bc3996fcb04e3629
ClayenKitten <dima070704@yandex.ru>
2024-08-27 16:25:29 +0000

    fix(frontend): invalidate state after email confirmation

--- web/src/routes/(main)/profile/(account)/confirmEmail/+page.svelte
+++ web/src/routes/(main)/profile/(account)/confirmEmail/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { invalidateAll, onNavigate } from "$app/navigation";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	onNavigate(async () => {
+		invalidateAll();
+	});
 </script>
 
 <header>
