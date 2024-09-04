3ccbc5c4b4dd23a6d9dc88aecb94bfcc747024fd
ClayenKitten <me@clayenkitten.dev>
2024-09-04 18:21:03 +0000

    feat(backend): don't use default SvelteKit logs for TRPC errors

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,9 +1,10 @@
 import appRouter from "$lib/server/router";
 import createContext from "$lib/server/context";
-import { redirect, type Handle } from "@sveltejs/kit";
+import { redirect, type Handle, type HandleServerError } from "@sveltejs/kit";
 import { createTRPCHandle } from "trpc-sveltekit";
 import { sequence } from "@sveltejs/kit/hooks";
 import { tokenCookieName } from "$lib/api";
+import { TRPCClientError } from "@trpc/client";
 
 const authorizationHandle: Handle = async ({ event, resolve }) => {
 	let pathname = event.url.pathname;
@@ -25,3 +26,15 @@ export const handle: Handle = sequence(
 	}),
 	authorizationHandle
 );
+
+export const handleError: HandleServerError = async ({
+	error,
+	status,
+	message
+}) => {
+	// Don't print default SvelteKit error for errors handled by TRPC.
+	if (error instanceof TRPCClientError) {
+		return;
+	}
+	return { error, status, message };
+};
