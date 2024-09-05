717595c5044b29c4d0dc450334e92a763d293095
ClayenKitten <me@clayenkitten.dev>
2024-09-05 14:53:46 +0000

    feat(backend): log all TRPC requests on the server

--- web/src/lib/server/router/trpc.ts
+++ web/src/lib/server/router/trpc.ts
@@ -1,11 +1,14 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import type { Context } from "../context";
 
-const {
-	router,
-	procedure: publicProcedure,
-	createCallerFactory
-} = initTRPC.context<Context>().create();
+const { router, procedure, createCallerFactory } = initTRPC
+	.context<Context>()
+	.create();
+
+const publicProcedure = procedure.use(opts => {
+	opts.ctx.logger.debug("TRPC request", { type: opts.type, path: opts.path });
+	return opts.next();
+});
 
 const protectedProcedure = publicProcedure.use(opts => {
 	if (!opts.ctx.session) {
