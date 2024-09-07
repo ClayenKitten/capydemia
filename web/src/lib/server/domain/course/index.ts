6b0ba879983943d64a6104d5b4ac78e10a3c26fc
ClayenKitten <me@clayenkitten.dev>
2024-09-07 20:33:35 +0000

    feat(backend): add course creation and update endpoints

--- web/src/lib/server/domain/course/index.ts
+++ web/src/lib/server/domain/course/index.ts
@@ -1,8 +1,10 @@
 import DbRepository from "$lib/server/db/repository";
 import type { Selectable } from "kysely";
 import type { User } from "../user";
-import type { Course as CourseModel } from "$lib/server/db/types";
+import type { Course as CourseTable } from "$lib/server/db/types";
+import * as m from "$lib/models";
 import type Result from "$lib/server/util/result";
+import { TRPCError } from "@trpc/server";
 
 export default class CourseService {
 	constructor(private repos: { course: CourseRepository }) {}
@@ -49,6 +51,140 @@ export class CourseRepository extends DbRepository {
 		return this.mapCourse(record);
 	}
 
+	public async createCourse(createCourse: m.CreateCourse): Promise<Course> {
+		let { id } = await this.db
+			.insertInto("course")
+			.values({
+				title: createCourse.title,
+				description: createCourse.description
+			})
+			.returning("id")
+			.executeTakeFirstOrThrow();
+		return new Course(id, createCourse.title, createCourse.description, []);
+	}
+
+	public async updateCourse(course: m.UpdateCourse): Promise<Course> {
+		return await this.db.transaction().execute(async trx => {
+			let result = await trx
+				.updateTable("course")
+				.set({ title: course.title, description: course.description })
+				.where("id", "=", course.id)
+				.executeTakeFirst();
+			if (result.numUpdatedRows === 0n) {
+				throw new TRPCError({ code: "NOT_FOUND" });
+			}
+
+			await deleteMissingModules(course.id, course.modules);
+			let modules = await Promise.all(
+				course.modules.map((module, i) => updateModule(course.id, module, i))
+			);
+			return new Course(course.id, course.title, course.description, modules);
+
+			async function deleteMissingModules(
+				courseId: number,
+				modules: m.updateModule[]
+			) {
+				if (course.modules.length !== 0) {
+					await trx
+						.deleteFrom("module")
+						.where(
+							"id",
+							"not in",
+							modules.map(({ id }) => id)
+						)
+						.execute();
+				} else {
+					await trx
+						.deleteFrom("module")
+						.where("courseId", "=", courseId)
+						.execute();
+				}
+			}
+
+			async function updateModule(
+				courseId: number,
+				module: m.updateModule,
+				order: number
+			): Promise<Module> {
+				if (module.id === null) {
+					let { id } = await trx
+						.insertInto("module")
+						.values({ courseId: courseId, title: module.title, order: order })
+						.returning("id")
+						.executeTakeFirstOrThrow();
+					console.log(id);
+					module.id = id;
+				} else {
+					let result = await trx
+						.updateTable("module")
+						.set({ title: module.title, order: order })
+						.where("id", "=", module.id)
+						.executeTakeFirst();
+					if (result.numUpdatedRows === 0n) {
+						throw new TRPCError({ code: "NOT_FOUND" });
+					}
+				}
+
+				let moduleId = module.id;
+				await deleteMissingLessons(moduleId, module.lessons);
+				let lessons = await Promise.all(
+					module.lessons.map((lesson, i) => updateLesson(moduleId, lesson, i))
+				);
+				return new Module(moduleId, module.title, lessons);
+			}
+
+			async function deleteMissingLessons(
+				moduleId: number,
+				lessons: m.UpdateLesson[]
+			) {
+				if (lessons.length !== 0) {
+					await trx
+						.deleteFrom("lesson")
+						.where(
+							"id",
+							"not in",
+							lessons.map(({ id }) => id)
+						)
+						.execute();
+				} else {
+					await trx
+						.deleteFrom("lesson")
+						.where("moduleId", "=", moduleId)
+						.execute();
+				}
+			}
+
+			async function updateLesson(
+				moduleId: number,
+				lesson: m.UpdateLesson,
+				order: number
+			) {
+				if (lesson.id === null) {
+					let { id } = await trx
+						.insertInto("lesson")
+						.values({ moduleId, title: lesson.title, order, content: {} })
+						.returning("id")
+						.executeTakeFirstOrThrow();
+					lesson.id = id;
+				} else {
+					let result = await trx
+						.updateTable("lesson")
+						.set({ title: lesson.title, order })
+						.where("id", "=", moduleId)
+						.executeTakeFirst();
+					if (result.numUpdatedRows === 0n) {
+						throw new TRPCError({ code: "NOT_FOUND" });
+					}
+				}
+				return new Lesson(lesson.id, lesson.title);
+			}
+		});
+	}
+
+	public async deleteCourse(courseId: number) {
+		await this.db.deleteFrom("course").where("id", "=", courseId).execute();
+	}
+
 	public async getEnrolledCourses(user: User): Promise<Course[]> {
 		return this.db
 			.selectFrom("courseEnrollment")
@@ -78,6 +214,14 @@ export class CourseRepository extends DbRepository {
 			.then(x => (x ? JSON.stringify(x.content) : null));
 	}
 
+	public async updateLessonContent(id: number, content: string): Promise<void> {
+		await this.db
+			.updateTable("lesson")
+			.set({ content })
+			.where("id", "=", id)
+			.execute();
+	}
+
 	public async getCourseByLesson(lessonId: number): Promise<Course | null> {
 		let courseRecord = await this.db
 			.selectFrom("lesson")
@@ -90,7 +234,7 @@ export class CourseRepository extends DbRepository {
 		return await this.mapCourse(courseRecord);
 	}
 
-	private async mapCourse(record: Selectable<CourseModel>): Promise<Course> {
+	private async mapCourse(record: Selectable<CourseTable>): Promise<Course> {
 		return new Course(
 			record.id,
 			record.title,
