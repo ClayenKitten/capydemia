0184968eb902fbb65fa2f7588d2cee3a3e463261
domik <astranastya_1@mail.ru>
2024-09-12 10:23:34 +0000

    feat(frontend): teacher profile created, edit course buttons created

--- web/src/routes/(main)/course/[courseId]/lesson/[lessonId]/+layout.svelte
+++ web/src/routes/(main)/course/[courseId]/lesson/[lessonId]/+layout.svelte
@@ -21,21 +21,57 @@
 	<div class="modules">
 		{#each data.course.modules as module, i}
 			<div class="module" class:current={module.id === data.module?.id}>
-				<button>
-					<span>Модуль {i + 1}. {module.title}</span>
-				</button>
+				<div class="module_header">
+					<button class="module_button">
+						<span>Модуль {i + 1}. {module.title}</span>
+					</button>
+					{#if data.user.isTeacher === true}
+						<div class="edit_buttons">
+							<button class="edit_module_name">
+								<img src="/icons/PencilSimple-32px.svg" alt="" />
+							</button>
+							<button class="delete_module">
+								<img src="/icons/Trash-32px.svg" alt="" />
+							</button>
+						</div>
+					{/if}
+				</div>
 				<div class="lessons">
 					{#each module.lessons as lesson, j}
-						<a
+						<div
+							class="lesson_header"
 							class:current={lesson.id === data.lesson?.id}
-							href={`/course/${data.course.id}/lesson/${lesson.id}`}
 						>
-							Урок {j + 1}. {lesson.title}
-						</a>
+							<a href={`/course/${data.course.id}/lesson/${lesson.id}`}>
+								Урок {j + 1}. {lesson.title}
+							</a>
+							{#if data.user.isTeacher === true}
+								<div class="edit_buttons">
+									<button class="edit_module_name">
+										<img src="/icons/PencilSimple-32px.svg" alt="" />
+									</button>
+									<button class="delete_module">
+										<img src="/icons/Trash-32px.svg" alt="" />
+									</button>
+								</div>
+							{/if}
+						</div>
 					{/each}
+					{#if data.user.isTeacher === true}
+						<button class="add_lesson">
+							<img src="/icons/Plus-24px.svg" alt="" />
+							Добавить урок
+						</button>
+					{/if}
 				</div>
 			</div>
 		{/each}
+		{#if data.user.isTeacher === true}
+			<button class="add_module">
+				<img src="/icons/Plus-24px.svg" alt="" />
+				Добавить модуль
+			</button>
+		{/if}
 	</div>
 	<div class="lesson">
 		<slot />
@@ -94,8 +130,54 @@
 		gap: 10px;
 		font: var(--B);
 
+		button {
+			border: none;
+			background-color: inherit;
+		}
+		.edit_buttons {
+			display: flex;
+			align-items: center;
+			gap: 6px;
+			padding: 12px 32px 12px 0;
+			button {
+				height: 24px;
+				width: 24px;
+				img {
+					filter: var(--filter-primary);
+				}
+			}
+		}
+
 		.module {
 			height: 72px;
+
+			.module_header {
+				display: flex;
+				justify-content: space-between;
+				height: 72px;
+				width: 100%;
+				background-color: var(--main-bg);
+				border: 1px solid var(--secondary);
+				border-radius: 8px;
+
+				.module_button {
+					width: 100%;
+					text-align: left;
+					padding: 12px 0 12px 32px;
+					border-radius: 8px;
+
+					> span {
+						color: var(--text);
+						max-width: 218px;
+						display: -webkit-box;
+						-webkit-line-clamp: 2;
+						line-clamp: 2;
+						text-overflow: ellipsis;
+						overflow: hidden;
+						-webkit-box-orient: vertical;
+					}
+				}
+			}
 
 			&:focus-within,
 			&.current {
@@ -109,56 +191,87 @@
 					border-top: none;
 					border-radius: 0 0 8px 8px;
 					padding: 12px 0 12px 0;
-					a {
+					.lesson_header {
 						display: flex;
 						align-items: center;
-						min-height: 60px;
-						padding: 10px 32px 10px 32px;
-						font: var(--P1);
-						color: var(--text);
-						line-height: 24px;
-						text-decoration: none;
+						a {
+							display: flex;
+							align-items: center;
+							min-height: 60px;
+							padding: 10px 0 10px 32px;
+							width: 100%;
+							font: var(--P1);
+							color: var(--text);
+							text-decoration: none;
+						}
 						&.current {
-							color: var(--primary);
+							background-color: var(--secondary);
 						}
 						&:not(.current):hover {
-							color: var(--secondary);
+							a {
+								color: var(--secondary);
+							}
 						}
 					}
 				}
-				button {
+				.module_header {
 					background-color: var(--primary);
 					color: var(--main-bg);
 					border-radius: 8px 8px 0 0;
 					border: none;
+
+					.module_button {
+						border-radius: 8px 8px 0 0;
+						span {
+							color: var(--main-bg);
+						}
+					}
+					.edit_buttons {
+						button {
+							img {
+								filter: var(--filter-main-bg);
+							}
+						}
+					}
 				}
 			}
 
-			button {
-				height: 72px;
-				padding: 12px 32px 12px 32px;
-				text-align: left;
-				width: 100%;
-				background-color: var(--main-bg);
-				border: 1px solid var(--secondary);
-				border-radius: 8px;
-
-				> span {
-					display: -webkit-box;
-					-webkit-line-clamp: 2;
-					line-clamp: 2;
-					text-overflow: ellipsis;
-					overflow: hidden;
-					-webkit-box-orient: vertical;
+			&:not(:focus-within, .current) > .module_header:hover {
+				span {
+					color: var(--secondary);
 				}
-			}
-
-			&:not(:focus-within, .current) > button:hover {
-				color: var(--secondary);
 			}
 
 			.lessons {
 				display: none;
+			}
+			.add_lesson {
+				display: flex;
+				gap: 8px;
+				justify-content: center;
+				align-items: center;
+				padding: 8px 16px 8px 0;
+				font: var(--B);
+				color: var(--primary);
+				img {
+					height: 16px;
+					width: 16px;
+					filter: var(--filter-primary);
+				}
+			}
+		}
+		.add_module {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			gap: 10px;
+			padding: 16px 16px 16px 0;
+			font: var(--B);
+			color: var(--primary);
+			img {
+				height: 16px;
+				width: 16px;
+				filter: var(--filter-primary);
 			}
 		}
 	}
