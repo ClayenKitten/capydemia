7412a01516dcc19403490011c65d9da7e259801e
domik <astranastya_1@mail.ru>
2024-10-02 17:22:17 +0000

    feat(frontend): Superfrms on auth pages added

--- web/src/routes/auth/recovery/new_password/+page.svelte
+++ web/src/routes/auth/recovery/new_password/+page.svelte
@@ -3,6 +3,32 @@
 	import api from "$lib/api";
 	import Button from "$lib/components/Button.svelte";
 	import Input from "$lib/components/Input.svelte";
+	import { z } from "zod";
+	import { superForm, defaults } from "sveltekit-superforms";
+	import { zod } from "sveltekit-superforms/adapters";
+	import { goto } from "$app/navigation";
+
+	const newPasswordSchema = z.object({
+		password: z
+			.string()
+			.min(8, "Пароль должен содержать хотя бы 8 символов")
+			.max(128, "Слишком длинный пароль"),
+		repeat_password: z.string()
+	});
+
+	const { form, errors, enhance, validateForm } = superForm(
+		defaults(zod(newPasswordSchema)),
+		{
+			SPA: true,
+			validators: zod(newPasswordSchema),
+			async onChange(event) {
+				valid =
+					(await validateForm()).valid &&
+					$form.password === $form.repeat_password;
+				password = $form.password;
+			}
+		}
+	);
 
 	async function submit() {
 		let code = $page.url.searchParams.get("code");
@@ -24,52 +50,68 @@
 			}
 		}
 	}
+
+	function complete() {
+		goto("/auth/sign_in");
+	}
+
 	let error: string | null = null;
 	let password = "";
-	let repeat_password = "";
 	let finish = false;
-
-	$: valid_password = password.length >= 8 && password.length <= 128;
-	$: valid_repeat_password = password === repeat_password;
-	$: valid = valid_password && valid_repeat_password;
+	let valid = false;
 </script>
 
 <main>
 	<div class="header">
 		<h4>Восстановление пароля</h4>
 	</div>
-	<div class="inputs">
-		<div class="type">
-			<label class="input" for={undefined}>
-				<span>Пароль</span>
-				<Input
-					type="password"
-					placeholder="Ваш пароль"
-					bind:value={password}
-					required
-					minlength={8}
-					maxlength={128}
-				/>
-			</label>
-			<label class="input" for={undefined}>
-				<span>Повторите пароль</span>
-				<Input
-					type="password"
-					placeholder="Повторите  пароль"
-					bind:value={repeat_password}
-					required
-					minlength={8}
-					maxlength={128}
-				/>
-			</label>
+	{#if !error}
+		<form use:enhance>
+			<div class="inputs">
+				<label class="input" for={undefined}>
+					<span>Пароль</span>
+					<Input
+						type="password"
+						placeholder="Ваш пароль"
+						bind:value={$form.password}
+						required
+						invalid={$errors.password ? true : false}
+					/>
+					{#if $errors.password}<span class="error">{$errors.password}</span
+						>{/if}
+				</label>
+				<label class="input" for={undefined}>
+					<span>Повторите пароль</span>
+					<Input
+						type="password"
+						placeholder="Повторите  пароль"
+						bind:value={$form.repeat_password}
+						required
+						invalid={$form.password !== $form.repeat_password ||
+						$errors.password
+							? true
+							: false}
+					/>
+					{#if $form.password !== $form.repeat_password}<span class="error"
+							>Пароли не совпадают</span
+						>{/if}
+				</label>
+			</div>
+			<Button
+				text="Подтвердить новый пароль"
+				kind="primary"
+				on:click={submit}
+				disabled={!valid}
+			/>
+		</form>
+	{:else if finish}
+		<div class="finish">
+			<span>Пароль успешно изменён</span>
+			<Button text="Войти" kind="primary" on:click={complete} />
 		</div>
-		<Button
-			text="Подтвердить новый пароль"
-			kind="primary"
-			on:click={submit}
-			disabled={password == "" || !valid}
-		/>
-	</div>
+	{:else}
+		<span class="error">{error}</span>
+	{/if}
 </main>
 
 <style lang="scss">
@@ -80,18 +122,33 @@
 		flex: 1;
 		align-items: center;
 		color: var(--text);
+		span {
+			font: var(--P2);
+			color: var(--text);
+		}
+		.error {
+			color: var(--error);
+			font: var(--P2);
+		}
+	}
+	.finish {
+		display: flex;
+		flex-direction: column;
+		gap: 28px;
+		text-align: center;
+		width: 100%;
 	}
 	h4 {
 		color: var(--text);
 		font: var(--H4);
 	}
-	.inputs {
+	form {
 		display: flex;
 		flex-direction: column;
 		color: var(--text-note);
 		gap: 36px;
 	}
-	.type {
+	.inputs {
 		display: flex;
 		flex-direction: column;
 		gap: 16px;
@@ -105,6 +162,10 @@
 		span {
 			font: var(--P2);
 			color: var(--text);
+		}
+		.error {
+			color: var(--error);
+			font: var(--P2);
 		}
 	}
 </style>
